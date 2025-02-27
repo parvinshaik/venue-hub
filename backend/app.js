4a52b0f46253933b7d54876b422bd779310df874
Esub Shaik <esubshaik7864@gmail.com>
2025-02-27 19:09:27 +0530

    added buffer pdf generation

--- backend/app.js
+++ backend/app.js
@@ -12,19 +12,11 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const mongoURI = process.env.MONGODB_URL;
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 30000, // 30 seconds (default is 10 seconds)
-  socketTimeoutMS: 45000, // 45 seconds (default is 30 seconds)
-  bufferCommands: false, // Optional: Disable buffering
-});
-
 mongoose
   .connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 450000, // 30 seconds (default is 10 seconds)
+    serverSelectionTimeoutMS: 450000,
     socketTimeoutMS: 45000,
     bufferCommands: false,
   })
