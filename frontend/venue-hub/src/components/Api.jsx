b19b65baf0eb650f972517581af8bc890bc69cf8
Esub Shaik <esubshaik7864@gmail.com>
2025-02-27 12:39:03 +0530

    fixed deployed backend api

--- frontend/venue-hub/src/components/Api.jsx
+++ frontend/venue-hub/src/components/Api.jsx
@@ -1,4 +1,3 @@
-// const api_url = "https://venue-hub-api.vercel.app/api/user" ;
-const api_url = "http://localhost:8000/api/user" ;
+const api_url = "https://venue-hub-api.vercel.app/api/user" ;
+// const api_url = "http://localhost:8000/api/user" ;
 export default api_url ;
-// new 90
